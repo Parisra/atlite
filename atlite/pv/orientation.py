Fix orientation.py: correct tilted horizontal tracking calculation
Fix the error in the formula calculating Azimuth difference for tilted horizontal tracking. The current formula results in a loss of generation after noon for locations in the global south.
For more details, see: https://docs.nrel.gov/docs/fy13osti/58891.pdf

atlite/pv/orientation.py
@@ -148,7 +148,7 @@ def SurfaceOrientation(ds, solar_position, orientation, tracking=None):
 
         azimuth_difference = sun_azimuth - surface_azimuth
         azimuth_difference = np.where(
-            azimuth_difference > pi, 2 * pi - azimuth_difference, azimuth_difference
+            azimuth_difference > pi , azimuth_difference - 2*pi , azimuth_difference
         )
         azimuth_difference = np.where(
             azimuth_difference < -pi, 2 * pi + azimuth_difference, azimuth_difference